08af8288909d9599609b1e4e14f247c18cbcb18d
Abhishek Kumar <abhishek82078@gmail.com>
2022-02-12 23:07:02 +0530

    Restricted from uploading other documents

--- drag_drop.js
+++ drag_drop.js
@@ -1,76 +1,71 @@
-document.querySelectorAll(".drag-drop-input").forEach((inputElement) =>{
-  const dragDropElement =inputElement.closest(".drag-drop");
+document.querySelectorAll(".drag-drop-input").forEach((inputElement) => {
+  const dragDropElement = inputElement.closest(".drag-drop");
 
-  dragDropElement.addEventListener("click", (e) =>{
+  dragDropElement.addEventListener("click", (e) => {
     inputElement.click();
   });
 
-  inputElement.addEventListener("change", (e) =>{
-    if(inputElement.files.length)
-    {
-      updatethumbnail(dragDropElement,inputElement.files[0]);
+  inputElement.addEventListener("change", (e) => {
+    if (inputElement.files.length) {
+      updatethumbnail(dragDropElement, inputElement.files[0]);
     }
   });
 
-  dragDropElement.addEventListener('dragover', (e) =>{
+  dragDropElement.addEventListener('dragover', (e) => {
     e.preventDefault();
     dragDropElement.classList.add("drag-drop-over");
   });
 
-  ["dragleave","dragend"].forEach((type) =>{
-     dragDropElement.addEventListener(type, (e) =>{
-       cout<<"hello";
-       dragDropElement.classList.remove("drag-drop-over");
-     });
-     
+  ["dragleave", "dragend"].forEach((type) => {
+    dragDropElement.addEventListener(type, (e) => {
+      dragDropElement.classList.remove("drag-drop-over");
+    });
+
   });
 
-  dragDropElement.addEventListener("drop",e=>{
+  dragDropElement.addEventListener("drop", e => {
     e.preventDefault();
 
-    if(e.dataTransfer.files.length)
-    {
-      inputElement.files=e.dataTransfer.files;
-      updatethumbnail( dragDropElement,e.dataTransfer.files[0]);
+    if (e.dataTransfer.files.length) {
+      inputElement.files = e.dataTransfer.files;
+      updatethumbnail(dragDropElement, e.dataTransfer.files[0]);
     }
   });
 
   dragDropElement.classList.remove("drag-drop-over");
 });
 
-function updatethumbnail(dragDropElement,file)
-{
-   let thumbnailElement = dragDropElement.querySelector(".drag-drop-thumb");
+function updatethumbnail(dragDropElement, file) {
+  let thumbnailElement = dragDropElement.querySelector(".drag-drop-thumb");
 
-   if(dragDropElement.querySelector('.drag-drop-tag'))
-     dragDropElement.querySelector('.drag-drop-tag').remove();
+  console.log(file);
 
-     if(dragDropElement.querySelector('.drag-drop-img'))
-     dragDropElement.querySelector('.drag-drop-img').remove();
+  if (file.type=="application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
+    const reader = new FileReader();
 
-   if(!thumbnailElement)
-   {
-     thumbnailElement = document.createElement('div');
-     thumbnailElement.classList.add("drag-drop-thumb");
-     dragDropElement.appendChild(thumbnailElement);
+    reader.readAsDataURL(file);
+    reader.onload = () => {
 
-   }
-    
-   
-   console.log(file);
-   thumbnailElement.dataset.label=file.name;
+      if (dragDropElement.querySelector('.drag-drop-tag'))
+        dragDropElement.querySelector('.drag-drop-tag').remove();
 
-   if(file.type.startsWith("application/"))
-   {
-     const reader = new FileReader();
+      if (dragDropElement.querySelector('.drag-drop-img'))
+        dragDropElement.querySelector('.drag-drop-img').remove();
 
-     reader.readAsDataURL(file);
-     reader.onload= () => {
-      console.log("hi")
-       thumbnailElement.style.backgroundImage=`url('image/docs.png')`;
-     };
-    }
-     else
-      thumbnailElement.style.backgroundImage = null;
-   
+
+      if (!thumbnailElement) {
+        thumbnailElement = document.createElement('div');
+        thumbnailElement.classList.add("drag-drop-thumb");
+        dragDropElement.appendChild(thumbnailElement);
+
+      }
+
+      thumbnailElement.style.backgroundImage = `url('image/docs.png')`;
+      thumbnailElement.dataset.label = file.name;
+
+    };
+  }
+  else
+    alert("You can only choose doc/docx file. Try uploading doc/docx file")
+
 }
